5506b3d6b6b8b663c1fb6751429265d0e1d6b294
david-lafontant <lafontant.david@gmail.com>
2022-01-11 17:45:58 -0500

    Add store class

--- script2.js
+++ script2.js
@@ -48,3 +48,29 @@ class displayBook {
 
   }
 }
+
+
+/ Store Class: Handles Storage
+class Store {
+  static getBooks() {
+      let books;
+      if (localStorage.getItem('books') === null) {
+          books = [];
+      } else {
+          books = JSON.parse(localStorage.getItem('books'));
+      }
+      return books;
+  }
+  static addBook(book) {
+      const books = Store.getBooks();
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(books));
+  }
+  static removeBook(elem) {
+      const books = Store.getBooks();
+      console.log(elem.id);
+      console.log(books[parseInt(elem.id)]);
+      books.splice(parseInt(elem.id), 1);
+           localStorage.setItem('books', JSON.stringify(books));
+  }
+}
