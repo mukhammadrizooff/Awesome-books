3c5d8f7812cbd68f6a9933be16259fe9857f7369
david-lafontant <lafontant.david@gmail.com>
2022-01-12 19:14:54 -0500

    Set up and link navigation script

--- clocks.js
+++ clocks.js
@@ -1,46 +1,43 @@
-
 function display_clock() {
-    let time = new Date()
+  const time = new Date();
 
-    let dayPeriod = "";
+  let dayPeriod = '';
 
-    let month = time.getMonth();
+  let month = time.getMonth();
 
-    let date = dateWithSuffix(time.getDate());
+  const date = dateWithSuffix(time.getDate());
 
-    let hours = time.getHours() % 12;
+  const hours = time.getHours() % 12;
 
-    if (time.getHours() > 12) {
-        dayPeriod = "PM";
+  if (time.getHours() > 12) {
+    dayPeriod = 'PM';
+  } else {
+    dayPeriod = 'AM';
+  }
+
+  function dateWithSuffix(date) {
+    const num1 = date % 10;
+    const num2 = date % 100;
+    if (num1 == date && num2 != 11) {
+      return `${date}st`;
     }
-    else {
-        dayPeriod = "AM";
+    if (num1 == 2 && num2 != 12) {
+      return `${date}nd`;
     }
-
-    function dateWithSuffix(date) {
-        let num1 = date % 10,
-            num2 = date % 100;
-        if (num1 == date && num2 != 11) {
-            return date + "st";
-        }
-        else if (num1 == 2 && num2 != 12) {
-            return date + "nd";
-        }
-        else if (num1 == 3 && num2 != 13) {
-            return date + "rd";
-        }
-        else {return date + "th";}
+    if (num1 == 3 && num2 != 13) {
+      return `${date}rd`;
     }
+    return `${date}th`;
+  }
 
-    let monthOfYear = ["January", "February", "March", "April", "May", "June", "July",
-        "August", "September", "October", "November", "December"];
+  const monthOfYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+    'August', 'September', 'October', 'November', 'December'];
 
-    month = monthOfYear[month];
+  month = monthOfYear[month];
 
-    let clock = month + " " + date + " " + time.getFullYear() + "," + " " + hours + ":" + time.getMinutes() + ":" + time.getSeconds() + " " + dayPeriod;
-    document.querySelector('.clock').innerHTML = clock;
+  const clock = `${month} ${date} ${time.getFullYear()},` + ` ${hours}:${time.getMinutes()}:${time.getSeconds()} ${dayPeriod}`;
+  document.querySelector('.clock').innerHTML = clock;
 }
 display_clock();
 
 setInterval(display_clock, 1000);
-
