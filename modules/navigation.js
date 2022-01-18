2dfed32386ecb1a4b352326cb4f237fb44e26a78
Your Name <legend.it.bk.ru>
2022-01-18 16:28:32 +0500

    Fix ESlinter errors

--- modules/navigation.js
+++ modules/navigation.js
@@ -1,49 +1,49 @@
 export default function navigation() {
-  const nav1 = document.querySelector("#list");
-  const nav2 = document.querySelector("#addNew");
-  const nav3 = document.querySelector("#contact");
+  const nav1 = document.querySelector('#list');
+  const nav2 = document.querySelector('#addNew');
+  const nav3 = document.querySelector('#contact');
 
-  const bookDisplaySection = document.querySelector("#bookSection");
-  const formDisplaySection = document.querySelector("#formSection");
-  const contactDisplaySection = document.querySelector("#contactSection");
+  const bookDisplaySection = document.querySelector('#bookSection');
+  const formDisplaySection = document.querySelector('#formSection');
+  const contactDisplaySection = document.querySelector('#contactSection');
 
-  nav1.addEventListener("click", () => {
-    if (bookDisplaySection.classList.contains("hidden")) {
-      bookDisplaySection.classList.remove("hidden");
+  nav1.addEventListener('click', () => {
+    if (bookDisplaySection.classList.contains('hidden')) {
+      bookDisplaySection.classList.remove('hidden');
     }
 
-    if (!formDisplaySection.classList.contains("hidden")) {
-      formDisplaySection.classList.add("hidden");
+    if (!formDisplaySection.classList.contains('hidden')) {
+      formDisplaySection.classList.add('hidden');
     }
 
-    if (!contactDisplaySection.classList.contains("hidden")) {
-      contactDisplaySection.classList.add("hidden");
-    }
-  });
-
-  nav2.addEventListener("click", () => {
-    if (formDisplaySection.classList.contains("hidden")) {
-      formDisplaySection.classList.remove("hidden");
-    }
-
-    if (!bookDisplaySection.classList.contains("hidden")) {
-      bookDisplaySection.classList.add("hidden");
-    }
-
-    if (!contactDisplaySection.classList.contains("hidden")) {
-      contactDisplaySection.classList.add("hidden");
+    if (!contactDisplaySection.classList.contains('hidden')) {
+      contactDisplaySection.classList.add('hidden');
     }
   });
 
-  nav3.addEventListener("click", () => {
-    if (contactDisplaySection.classList.contains("hidden")) {
-      contactDisplaySection.classList.remove("hidden");
+  nav2.addEventListener('click', () => {
+    if (formDisplaySection.classList.contains('hidden')) {
+      formDisplaySection.classList.remove('hidden');
     }
-    if (!bookDisplaySection.classList.contains("hidden")) {
-      bookDisplaySection.classList.add("hidden");
+
+    if (!bookDisplaySection.classList.contains('hidden')) {
+      bookDisplaySection.classList.add('hidden');
     }
-    if (!formDisplaySection.classList.contains("hidden")) {
-      formDisplaySection.classList.add("hidden");
+
+    if (!contactDisplaySection.classList.contains('hidden')) {
+      contactDisplaySection.classList.add('hidden');
+    }
+  });
+
+  nav3.addEventListener('click', () => {
+    if (contactDisplaySection.classList.contains('hidden')) {
+      contactDisplaySection.classList.remove('hidden');
+    }
+    if (!bookDisplaySection.classList.contains('hidden')) {
+      bookDisplaySection.classList.add('hidden');
+    }
+    if (!formDisplaySection.classList.contains('hidden')) {
+      formDisplaySection.classList.add('hidden');
     }
   });
 }
