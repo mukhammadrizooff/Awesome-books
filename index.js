350446f6ab90b7c3c4eda2b79c3af84c39d4ca27
Your Name <legend.it.bk.ru>
2022-01-18 16:09:34 +0500

    Fix linter errors

--- index.js
+++ index.js
@@ -5,46 +5,46 @@ import displayBook from './modules/display.js';
 
 navigation();
 
- // Event: Display Books
- document.addEventListener('DOMContentLoaded', displayBook.displayBooks);
- let increment = 0;
- 
- // Event: Add a Book
- document.querySelector('#formId').addEventListener('submit', (e) => {
-   // Prevent actual submit
-   e.preventDefault();
- 
-   // Get form values
-   const title = document.querySelector('#title').value;
-   const author = document.querySelector('#author').value;
-   const id = increment;
- 
-   increment++;
- 
-   // Validate
-   if (title === '' || author === '') {
-     alert('Please fill in all fields');
-   } else {
-     // Instatiate book
-     const book = new Book(title, author, id);
- 
-     // Add Book to displayBook
-     displayBook.addBookToList(book);
- 
-     // Add book to store
-     Store.addBook(book);
- 
-     // Clear fields
-     displayBook.clearFields();
-   }
- });
- 
- // Event: Remove a Book
- document.querySelector('#listBooks').addEventListener('click', (e) => {
-   // Remove book from displayBook
-   displayBook.deleteBook(e.target);
- 
-   // Remove book from store
-   Store.removeBook(e.target);
- });
+// Event: Display Books
+document.addEventListener('DOMContentLoaded', displayBook.displayBooks);
+let increment = 0;
+
+// Event: Add a Book
+document.querySelector('#formId').addEventListener('submit', (e) => {
+  // Prevent actual submit
+  e.preventDefault();
+
+  // Get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const id = increment;
+
+  increment++;
+
+  // Validate
+  if (title === '' || author === '') {
+    alert('Please fill in all fields');
+  } else {
+    // Instantiate book
+    const book = new Book(title, author, id);
+
+    // Add Book to displayBook
+    displayBook.addBookToList(book);
+
+    // Add book to store
+    Store.addBook(book);
+
+    // Clear fields
+    displayBook.clearFields();
+  }
+});
+
+// Event: Remove a Book
+document.querySelector('#listBooks').addEventListener('click', (e) => {
+  // Remove book from displayBook
+  displayBook.deleteBook(e.target);
+
+  // Remove book from store
+  Store.removeBook(e.target);
+});
  
--- modules/display.js
+++ modules/display.js
@@ -1,32 +1,32 @@
 import Store from './store.js';
 
 export default class displayBook {
-    static displayBooks() {
-      const books = Store.getBooks();
-  
-      books.forEach((book) => displayBook.addBookToList(book));
-    }
-  
-    static addBookToList(book) {
-      const elementList = document.getElementById('listBooks');
-      const listItem = document.createElement('li');
-  
-      listItem.innerHTML = `
+  static displayBooks() {
+    const books = Store.getBooks();
+
+    books.forEach((book) => displayBook.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const elementList = document.getElementById('listBooks');
+    const listItem = document.createElement('li');
+
+    listItem.innerHTML = `
       <span>"${book.title}" by ${book.author}</span>
       <button type='button' id='${book.id}' class='remove-btn'>Remove</button>
       `;
-  
-      elementList.appendChild(listItem);
-    }
-  
-    static deleteBook(el) {
-      if (el.classList.contains('remove-btn')) {
-        el.parentElement.remove();
-      }
-    }
-  
-    static clearFields() {
-      document.querySelector('#title').value = '';
-      document.querySelector('#author').value = '';
+
+    elementList.appendChild(listItem);
+  }
+
+  static deleteBook(el) {
+    if (el.classList.contains('remove-btn')) {
+      el.parentElement.remove();
     }
   }
+
+  static clearFields() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+  }
+}
